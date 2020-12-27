93ce1f37b14c24d5e5abc280033c89a094060b86
Khawaja Hassaan Arif <hassaanarif.ha@gmail.com>
2020-12-28 02:37:15 +0500

    Addes HashMaps

--- hashMap.js
+++ hashMap.js
@@ -1,34 +1,37 @@
 class HashMap {
-  constructor(data) {
-    this.data = data;
-    this.map = {};
+  constructor(hashTableSize) {
+    this.hashTablesize = hashTableSize;
+    this.hashArray = new Array(hashTableSize);
   }
 
-  insert = () => {
-    for (let i = 0; i < this.data.length; i++) {
-      let character = this.data.substr(i, 1);
-      //checking if character is present in hashtable
-      if (this.map[character]) {
-        //increasing the count by one
-        this.map[character] = this.map[character] + 1;
-      } else {
-        this.map[character] = 1;
-      }
+  hashFunction = (key) => {
+    let hash = 83;
+    if (key == null) {
+      return;
     }
-    console.log(this.map);
+    for (let char of key) {
+      hash =
+        (12223242526272829 * hash * char.charCodeAt(0)) % this.hashTablesize;
+    }
+    return hash;
   };
 
-  lookup = () => {
-    for (let i = 0; i < this.data.length; i++) {
-      let character = this.data.substr(i, 1);
-      if (this.map[character] == 1) {
-        console.log(character);
-        return;
-      }
+  get = (key) => {
+    let index = this.hashFunction(key);
+    return this.hashArray[index];
+  };
+
+  set = (key, value) => {
+    let index = this.hashFunction(key);
+    this.hashArray[index] = value;
+  };
+
+  contains = (key) => {
+    let index = this.hashFunction(key);
+    if (this.hashArray[index]) {
+      return true;
+    } else {
+      return false;
     }
   };
 }
-
-let map = new HashMap("A Green Apple");
-map.insert();
-map.lookup();
--- hashMap.py
+++ hashMap.py
@@ -1,7 +1,30 @@
 class HashMap:
-    def __init__(self):
-        self
+    def __init__(self, hashTableSize):
+        self.hashTableSize = hashTableSize
+        self.hashTable = [None] * hashTableSize
 
-    def insert(data):
-        for i in data:
-            
+
+    def hashFunction(self, key):
+        hashValue = 83
+        if key == None:
+            return
+
+        for char in key:
+            hashValue = (12223242526272829 * hashValue * ord(char)) % self.hashTableSize
+
+        return hashValue
+
+    def setValue(self, key, value):
+        index = self.hashFunction(key)
+        self.hashTable[index] = value
+    
+    def getValue(self, key, value):
+        index = self.hashFunction(key)
+        return self.hashTable[index]
+
+    def contains(self, key):
+        index = self.hashFunction(key)
+        if self.hashTable[index]:
+            return True
+        else:
+            return False
